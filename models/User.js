1ec697d55dcee95b01969f22ab12a38d75864015
RamsesCastillo42 <ramsescastillo42@gmail.com>
2018-12-17 15:21:09 -0500

    refactor

--- models/User.js
+++ models/User.js
@@ -1,16 +1,34 @@
 var keystone = require('keystone');
+var Types = keystone.Field.Types;
 
+/**
+ * User Model
+ * ==========
+ */
 var User = new keystone.List('User');
 
 User.add({
-  displayName: {type: String },
-  email: { type: keystone.Field.Types.Email, unique: true },
-  password: { type: keystone.Field.Types.Password },
+	name: { type: Types.Name, required: true, index: true },
+	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
+	password: { type: Types.Password, initial: true, required: true },
+}, 'Permissions', {
+	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 });
 
+// Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function () {
-  return true;
+	return this.isAdmin;
 });
 
-User.defaultColumns = 'id, displayName, email';
+
+/**
+ * Relationships
+ */
+User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
+
+
+/**
+ * Registration
+ */
+User.defaultColumns = 'name, email, isAdmin';
 User.register();
--- updates/0.0.1-first-user.js
+++
@@ -1,8 +0,0 @@
-exports.create = {
-    User: [{
-        displayName: "Mestiza",
-        email: "admin@keystonejs.com",
-        password: "admin",
-    }
-  ],
-};
